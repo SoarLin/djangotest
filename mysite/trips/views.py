c890762733e81eac78d0092de011f9b7a1eb3350
Soar <soar.lin@gmail.com>
2015-03-15 15:04:04 +0800

    [Add]新增trips這個app

--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -4,18 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_world(request):
-    # return HttpResponse("Hello World!!")
-    output = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>另北的Django網頁</title>
-    </head>
-    <body>
-      <h2>It's Works!</h2>
-      <p>Hello World!! <em style="color:LightSeaGreen;">{current_time}</em></p>
-    </body>
-    </html>
-    """.format(current_time=datetime.now())
-
-    return HttpResponse(output)
+    return render(request,
+                  'hello_world.html',
+                  {'current_time': datetime.now()})

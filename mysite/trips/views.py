df8b0d9ffe20b59442b0d6826cf0de8c110a848e
Soar <soar.lin@gmail.com>
2015-03-15 15:41:13 +0800

    [Add]新增基本CRUD頁面

--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
+from trips.models import Post
 
 # Create your views here.
 def hello_world(request):
     return render(request,
                   'hello_world.html',
                   {'current_time': datetime.now()})
+
+def home(request):
+    # get all posts
+    post_list = Post.objects.all()
+    return render(request,
+                'home.html',
+                {'post_list': post_list})
